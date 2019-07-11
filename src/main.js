cdb3bcc6623c91a41e3cce501a4e4097c4927826
zrh <2864700929@qq.com>
2019-07-11 22:10:41 +0800

    登陆完成

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 // 导入element-ui
 import ElementUI from 'element-ui'
+import axios from 'axios'
 import 'element-ui/lib/theme-chalk/index.css'
 // 1. 默认索引  文件夹下 存在 index.js index.vue index.json 默认加载这些文件
 // 2. 在使用vue-cli的时候 @符号在路径中作为前缀使用的时候 代表src目录
 import router from './router/index.js'
+Vue.prototype.axios = axios
 Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,8 +2,20 @@
     <div class="login-container">
         <el-card class="login-box">
             <img src="../../assets/images/logo_index.png" alt="">
-            <el-form>
-
+            <el-form :model="loginform" :rules="rules" :status-icon="true" ref="loginform">
+                <el-form-item prop="mobile">
+                    <el-input v-model="loginform.mobile" placeholder="请输入手机号" ></el-input>
+                </el-form-item>
+                  <el-form-item prop="code">
+                    <el-input v-model="loginform.code" placeholder="请输入验码" style="width:240px"></el-input>
+                    <el-button style="float:right">发送验证码</el-button>
+                </el-form-item>
+                  <el-form-item>
+                    <el-checkbox :value="true" >我已阅读并同意用户协议和隐私条款</el-checkbox>
+                </el-form-item>
+                 <el-form-item>
+                   <el-button style="width:100%" type="primary" @click="login()">登陆</el-button>
+                </el-form-item>
             </el-form>
         </el-card>
     </div>
@@ -12,7 +24,50 @@
 <script>
 
 export default {
-
+  data () {
+    // 验证手机号
+    const checkmobile = (rule, value, callback) => {
+      // 校验逻辑
+      if (/^1[3-9]\d{9}$/.test(value)) {
+        callback()
+      } else {
+        callback(new Error('手机格式不正确!'))
+      }
+    }
+    return {
+      loginform: {
+        mobile: '',
+        code: ''
+      },
+      rules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { validator: checkmobile, trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    login () {
+      this.$refs.loginform.validate((valid) => {
+        if (valid) {
+          // 提交登陆请求
+          this.axios.post('http://ttapi.research.itcast.cn/mp/v1_0/authorizations', this.loginform)
+            .then((res) => {
+              // 后台返回的数据
+              // console.log(res.data)
+              this.$router.push('/')
+            })
+            .catch(() => {
+              this.message.error('手机号或验证码错误')
+            })
+        }
+      })
+    }
+  }
 }
 </script>
 
@@ -26,7 +81,7 @@ export default {
   background: url(../../assets/images/login_bg.jpg) no-repeat center/cover;
   .login-box{
     width:400px;
-    height:300px;
+    height:340px;
     position: absolute;
     left:50%;
     top:50%;
